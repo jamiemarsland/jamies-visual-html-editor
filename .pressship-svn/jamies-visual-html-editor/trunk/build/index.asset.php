b7307f2fe7ca43b9b07a46e70f0e76a80bbb7234
jamiemarsland <jamiemarsland@pootlepress.co.uk>
2026-07-01 06:10:23 +0100

    Stop tracking local SVN working copy

--- .pressship-svn/jamies-visual-html-editor/trunk/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-media-utils'), 'version' => '44fd77c452c7ef50a51b');
